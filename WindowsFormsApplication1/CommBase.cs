4c7784983c2ffc05855256ce4f0bb9eeeb402904
Sambeat <minisylux@gmail.com>
2015-10-05 07:38:14 -0400

    Meh

---
+++ WindowsFormsApplication1/CommBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class CommBase : INotifyPropertyChanged
+    {
+        private const int DELAI = 200;
+        private ServerConnection serverConnection;
+        private static CommBase commBaseInstance = null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CommBase()
+        {
+
+        }
+
+
+
+        
+    }
+}
